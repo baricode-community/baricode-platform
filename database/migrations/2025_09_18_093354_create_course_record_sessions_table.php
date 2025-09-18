3f2fb0636c85c13cb31844d0a9ecfcc632737f5d
ahlaiptek <iptekahla@gmail.com>
2025-09-18 16:40:45 +0700

    Add CourseRecordSession model and migration for session tracking

---
+++ database/migrations/2025_09_18_093354_create_course_record_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('course_record_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_record_id');
+            $table->unsignedTinyInteger('day_of_week')->comment('1=Ahad, 2=Senin, 3=Selasa, 4=Rabu, 5=Kamis, 6=Jumat, 7=Sabtu');
+
+            $table->time('reminder_1');
+            $table->time('reminder_2')->nullable();
+            $table->time('reminder_3')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('course_record_id')
+                ->references('id')->on('course_records')
+                ->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('course_record_sessions');
+    }
+};
