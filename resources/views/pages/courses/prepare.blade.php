9dd05a1dea95d0e59a3f3917744e8ba837aae5ef
ahlaiptek <iptekahla@gmail.com>
2025-09-20 17:22:35 +0700

    Update course preparation form to include multiple session time inputs for better scheduling flexibility

--- resources/views/pages/courses/prepare.blade.php
+++ resources/views/pages/courses/prepare.blade.php
@@ -36,13 +36,26 @@
                                     </span>
                                 </label>
                                 <div class="flex space-x-2 items-center">
-                                    <input type="time" name="days[{{ $day }}][start_time]"
-                                        class="form-input rounded-md border-gray-300 dark:bg-gray-800 dark:border-gray-600 day-time-{{ $day }} px-3 py-2 w-28 text-indigo-700 dark:text-indigo-200 bg-white dark:bg-gray-800 shadow-inner transition focus:ring-2 focus:ring-indigo-400"
-                                        disabled>
-                                    <span class="text-gray-400 dark:text-gray-500">-</span>
-                                    <input type="time" name="days[{{ $day }}][end_time]"
-                                        class="form-input rounded-md border-gray-300 dark:bg-gray-800 dark:border-gray-600 day-time-{{ $day }} px-3 py-2 w-28 text-indigo-700 dark:text-indigo-200 bg-white dark:bg-gray-800 shadow-inner transition focus:ring-2 focus:ring-indigo-400"
-                                        disabled>
+                                    <div class="flex flex-col items-center">
+                                        <input type="time" name="days[{{ $day }}][sesi_1]"
+                                            class="form-input rounded-md border-gray-300 dark:bg-gray-800 dark:border-gray-600 day-time-{{ $day }} px-3 py-2 w-28 text-indigo-700 dark:text-indigo-200 bg-white dark:bg-gray-800 shadow-inner transition focus:ring-2 focus:ring-indigo-400"
+                                            disabled>
+                                        <span class="text-xs text-gray-400 dark:text-gray-500 mt-1">Sesi 1</span>
+                                    </div>
+                                    <span class="text-gray-400 dark:text-gray-500">,</span>
+                                    <div class="flex flex-col items-center">
+                                        <input type="time" name="days[{{ $day }}][sesi_2]"
+                                            class="form-input rounded-md border-gray-300 dark:bg-gray-800 dark:border-gray-600 day-time-{{ $day }} px-3 py-2 w-28 text-indigo-700 dark:text-indigo-200 bg-white dark:bg-gray-800 shadow-inner transition focus:ring-2 focus:ring-indigo-400"
+                                            disabled>
+                                        <span class="text-xs text-gray-400 dark:text-gray-500 mt-1">Sesi 2</span>
+                                    </div>
+                                    <span class="text-gray-400 dark:text-gray-500">,</span>
+                                    <div class="flex flex-col items-center">
+                                        <input type="time" name="days[{{ $day }}][sesi_3]"
+                                            class="form-input rounded-md border-gray-300 dark:bg-gray-800 dark:border-gray-600 day-time-{{ $day }} px-3 py-2 w-28 text-indigo-700 dark:text-indigo-200 bg-white dark:bg-gray-800 shadow-inner transition focus:ring-2 focus:ring-indigo-400"
+                                            disabled>
+                                        <span class="text-xs text-gray-400 dark:text-gray-500 mt-1">Sesi 3</span>
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
