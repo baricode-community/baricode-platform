b9cc6326b3c0f619cd9c0970d2378a287923f9eb
ahlaiptek <iptekahla@gmail.com>
2025-09-21 16:21:39 +0700

    Remove fillable attributes from User model and retain guarded attributes for improved security

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,16 +17,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
