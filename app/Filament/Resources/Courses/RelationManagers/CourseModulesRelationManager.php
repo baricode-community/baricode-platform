a43a6de91ba4d3b213bc98032c9ced00c780a78d
ahlaiptek <iptekahla@gmail.com>
2025-09-24 11:21:12 +0700

    Add CourseModulesRelationManager for managing course modules with CRUD operations

---
+++ app/Filament/Resources/Courses/RelationManagers/CourseModulesRelationManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources\Courses\RelationManagers;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CourseModulesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'courseModules';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('order')
+                    ->required()
+                    ->numeric()
+                    ->unique()
+                    ->minValue(1),
+                TextInput::make('description')
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('order')
+                    ->sortable(),
+            ])
+            ->defaultSort('order')
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
