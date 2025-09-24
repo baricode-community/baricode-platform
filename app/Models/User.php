d3c84b13d90298124fda014c3700e46282ffde0b
ahlaiptek <iptekahla@gmail.com>
2025-09-24 08:57:57 +0700

    Fix User model implementation by ensuring MustVerifyEmail is correctly implemented and removing duplicate Notifiable trait usage.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,10 +14,10 @@ use Spatie\Permission\Traits\HasRoles;
 
 
 #[ObservedBy([UserObserver::class])]
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, Notifiable;
 
     protected $guarded = ['id'];
 
