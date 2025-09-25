118ac818e66049d83f175760e65131241b0b0aff
ahlaiptek <iptekahla@gmail.com>
2025-09-25 21:39:06 +0700

    Add top navigation to Admin Panel and define navigation group for UserResource

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -15,12 +15,14 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use UnitEnum;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static UnitEnum|string|null $navigationGroup = 'Manajemen Pengguna';
 
     public static function form(Schema $schema): Schema
     {
