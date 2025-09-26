1935a1ce4697284e28f6484fb3200c5d8575f470
ahlaiptek <iptekahla@gmail.com>
2025-09-26 16:26:29 +0700

    Filter out unverified users in UserController index method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::where(['email_verified_at' => null])->get();
+        $users = User::whereNotNull('email_verified_at')->get();
         return view('pages.users.index', compact('users'));
     }
 }
