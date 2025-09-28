4eae9bb421b7745ad3c7998a493b7fbd15939445
ahlaiptek <iptekahla@gmail.com>
2025-09-29 06:41:05 +0700

    fix: Remove unnecessary SVG icon from meet link button for cleaner UI

--- resources/views/livewire/meets/index.blade.php
+++ resources/views/livewire/meets/index.blade.php
@@ -257,9 +257,6 @@ new #[Layout('components.layouts.app')] class extends Component {
                                     @if($meet->meet_link || $meet->youtube_link)
                                         <button wire:click="openMeetLink({{ $meet->id }})" 
                                                 class="px-4 py-2 text-sm font-medium text-white bg-green-600 hover:bg-green-700 dark:bg-green-700 dark:hover:bg-green-800 rounded-md transition-colors">
-                                            <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-2M7 9l3 3 3-3m4 4V9a4 4 0 00-8 0v3"/>
-                                            </svg>
                                             Masuk
                                         </button>
                                     @endif
