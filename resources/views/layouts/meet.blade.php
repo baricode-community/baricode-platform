003b149b79c3d801f6bbdcd78f289bb9df32c9e0
ahlaiptek <iptekahla@gmail.com>
2025-10-16 06:58:10 +0700

    feat: Add meta tags for description and keywords in meet layout

--- resources/views/layouts/meet.blade.php
+++ resources/views/layouts/meet.blade.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Komunitas Ngoding - 100% Gratis')</title>
+    
+    <meta name="description" content="{{ $meet->description ?? 'Bergabunglah dengan komunitas ngoding kami untuk belajar, berbagi, dan berkembang bersama. 100% gratis!' }}">
+    <meta name="keywords" content="komunitas ngoding, belajar ngoding, coding gratis, pemrograman, developer, web development, programming community">
+
     <link rel="icon" type="image/png" href="{{ asset('favicon.svg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @fluxAppearance
