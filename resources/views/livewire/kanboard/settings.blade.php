051662cac09b27fd2f6b97f7c877551efacd4391
ahlaiptek <iptekahla@gmail.com>
2025-10-30 23:36:29 +0700

    fix: Remove email display from user invite list for improved privacy

--- resources/views/livewire/kanboard/settings.blade.php
+++ resources/views/livewire/kanboard/settings.blade.php
@@ -467,7 +467,6 @@ new #[Layout('layouts.app')] class extends Component {
                                     <div class="flex justify-between items-center">
                                         <div>
                                             <p class="font-medium">{{ $user['name'] }}</p>
-                                            <p class="text-xs text-gray-500 dark:text-gray-400">{{ $user['email'] }}</p>
                                         </div>
                                         <svg class="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
