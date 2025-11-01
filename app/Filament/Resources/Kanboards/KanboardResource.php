93bd2bf75065669ed5895c315d77c1f30fb09047
ahlaiptek <iptekahla@gmail.com>
2025-11-01 15:54:35 +0700

    feat: Implement Kanboard resource with Create, Edit, List functionalities and associated schemas and tables

---
+++ app/Filament/Resources/Kanboards/KanboardResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Resources\Kanboards;
+
+use App\Filament\Resources\Kanboards\Pages\CreateKanboard;
+use App\Filament\Resources\Kanboards\Pages\EditKanboard;
+use App\Filament\Resources\Kanboards\Pages\ListKanboards;
+use App\Filament\Resources\Kanboards\Schemas\KanboardForm;
+use App\Filament\Resources\Kanboards\Tables\KanboardsTable;
+use App\Models\Kanboard;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KanboardResource extends Resource
+{
+    protected static ?string $model = Kanboard::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    public static function form(Schema $schema): Schema
+    {
+        return KanboardForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return KanboardsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListKanboards::route('/'),
+            'create' => CreateKanboard::route('/create'),
+            'edit' => EditKanboard::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Kanboards/Pages/CreateKanboard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Kanboards\Pages;
+
+use App\Filament\Resources\Kanboards\KanboardResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateKanboard extends CreateRecord
+{
+    protected static string $resource = KanboardResource::class;
+}
