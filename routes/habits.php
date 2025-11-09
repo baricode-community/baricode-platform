c1272956447752dd05160064b080f2656be83b31
ahlaiptek <iptekahla@gmail.com>
2025-11-09 20:27:43 +0700

    fix: Reorder habit show route for consistency in routing structure

--- routes/habits.php
+++ routes/habits.php
@@ -9,10 +9,10 @@ Route::middleware(['auth', 'verified'])
         // Main pages
         Volt::route('/', 'habits.index')->name('satu-tapak.index');
         Volt::route('create', 'habits.create')->name('satu-tapak.create');
-        Volt::route('/{habitId}', 'habits.show')->name('satu-tapak.show');
         
         // Invitations
         Volt::route('invitations', 'habits.invitations')->name('satu-tapak.invitations.index');
+        Volt::route('/{habitId}', 'habits.show')->name('satu-tapak.show');
         
         // Habit specific actions
         Volt::route('/{habitId}/invite', 'habits.invite')->name('satu-tapak.invite');
