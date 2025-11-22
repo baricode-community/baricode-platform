e66073d06ed06447db800a3dd9ea0207df6cdcb7
ahlaiptek <iptekahla@gmail.com>
2025-11-23 00:10:46 +0700

    Remove unused habit-related views: invitations, invite, show, statistics

--- backup/habits-views/invite.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('components.layouts.app')
-
-@section('title', 'Undang Teman - ' . $habit->name)
-
-@section('content')
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-    <livewire:habit-invite :habit="$habit" />
-</div>
-@endsection
